5dde4897dfda3304a3d6c59a397c71e9bac6c17b
maximeboinet <maximeboinet@live.com>
2017-07-23 15:55:42 +0200

    graph game ok

--- src/sample/view/GameController.java
+++ src/sample/view/GameController.java
@@ -5,8 +5,7 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.chart.PieChart;
-import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.*;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -15,6 +14,8 @@ import sample.model.AssossiativScorePlayer;
 import sample.model.Game;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 
 import static javafx.collections.FXCollections.*;
@@ -79,6 +80,7 @@ public class GameController {
     }
 
     private void initData() {
+        System.out.print("Ok1");
         int cptplayed = Main.Games.values().size();
         int cptscore = 0;
         int totplayed = 0;
@@ -86,20 +88,90 @@ public class GameController {
         for (Game g: Main.Games.values()) {
             totplayed += g.getScore().length;
             cptscore += g.getScore().length;
-            for (AssossiativScorePlayer ass: g.getScore()) {
+            for (AssossiativScorePlayer ass: g.getScore())
                 totscore += Main.Scores.get(ass.getScore()).getScoreInGame();
-            }
         }
         int averagePlayed = totplayed/cptplayed;
         int averageScore = totscore/cptscore;
-        int abovePlayed = 0;
-        int intoAveragePlayed = 0;
-        int belowPlayed = 0;
-        int aboveScore = 0;
-        int intoAverageScore = 0;
-        int belowScore = 0;
+        averageplayed.setText(String.valueOf(averagePlayed));
+        averagescore.setText(String.valueOf(averageScore));
+        int[][] sbc = {{0,0,0},{0,0,0},{0,0,0}};
+        for (Game g: Main.Games.values()) {
+            for (AssossiativScorePlayer ass: g.getScore()) {
+                int score = Main.Scores.get(ass.getScore()).getScoreInGame();
+                if (g.getScore().length > averagePlayed) {
+                    if ( score > averageScore)
+                        sbc[2][2] ++;
+                    else if (score < averageScore)
+                        sbc[2][0] ++;
+                    else
+                        sbc[2][1] ++;
+                } else if(g.getScore().length < averagePlayed) {
+                    if ( score > averageScore)
+                        sbc[0][2] ++;
+                    else if (score < averageScore)
+                        sbc[0][0] ++;
+                    else
+                        sbc[0][1] ++;
+                } else {
+                    if ( score > averageScore)
+                        sbc[1][2] ++;
+                    else if (score < averageScore)
+                        sbc[1][0] ++;
+                    else
+                        sbc[1][1] ++;
+                }
+            }
+        }
+        initPieChartScore(sbc);
+        initPieChartPlayed(sbc);
+        initStackedBar(sbc, totplayed, totscore);
+    }
 
+    private void initStackedBar(int[][] sbc, int totplayed, int totscore) {
+        XYChart.Series<String, Number> series1 = new XYChart.Series<>();
+        XYChart.Series<String, Number> series2 = new XYChart.Series<>();
+        XYChart.Series<String, Number> series3 = new XYChart.Series<>();
+        CategoryAxis xAxis = new CategoryAxis();
+        NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Played");
+        xAxis.setCategories(FXCollections.observableArrayList(Arrays.asList("Played below av", "Played av", "Played above av")));
+        yAxis.setLabel("Value %");
+        averageplayscor.setTitle("Average played by average score");
+        series1.setName("Score below av");
+        series1.getData().add(new XYChart.Data<>("Played below av", sbc[0][0]* (100/totplayed)));
+        series1.getData().add(new XYChart.Data<>("Played av", sbc[1][0]* (100/totplayed)));
+        series1.getData().add(new XYChart.Data<>("Played above av", sbc[2][0]* (100/totplayed)));
+        series2.setName("Score average");
+        series2.getData().add(new XYChart.Data<>("Played below av", sbc[0][1]* (100/totplayed)));
+        series2.getData().add(new XYChart.Data<>("Played av", sbc[1][1]* (100/totplayed)));
+        series2.getData().add(new XYChart.Data<>("Played above av", sbc[2][1]* (100/totplayed)));
+        series3.setName("Score above av");
+        series3.getData().add(new XYChart.Data<>("Played below av", sbc[0][2]* (100/totplayed)));
+        series3.getData().add(new XYChart.Data<>("Played av", sbc[1][2]* (100/totplayed)));
+        series3.getData().add(new XYChart.Data<>("Played above av", sbc[2][2]* (100/totplayed)));
+        averageplayscor.getData().addAll(series1,series2,series3);
 
+    }
+
+    private void initPieChartScore(int[][] sbc) {
+        avscoreobs =
+                FXCollections.observableArrayList(
+                        new PieChart.Data("AboveAverage", sbc[0][2]+sbc[1][2]+sbc[2][2]),
+                        new PieChart.Data("Average", sbc[0][1]+sbc[1][1]+sbc[2][1]),
+                        new PieChart.Data("BelowAverage", sbc[0][0]+sbc[1][0]+sbc[2][0]));
+        avscopie.setData(avscoreobs);
+        avscopie.setTitle("Score average");
+    }
+
+    private void initPieChartPlayed(int[][] sbc) {
+        avgames =
+                FXCollections.observableArrayList(
+                        new PieChart.Data("AboveAverage", sbc[2][0]+sbc[2][1]+sbc[2][2]),
+                        new PieChart.Data("Average", sbc[1][0]+sbc[1][1]+sbc[1][2]),
+                        new PieChart.Data("BelowAverage", sbc[0][0]+sbc[0][1]+sbc[0][2]));
+        avplaypie.setData(avgames);
+        avplaypie.setTitle("Played average");
     }
 
     private void initLabel() {
